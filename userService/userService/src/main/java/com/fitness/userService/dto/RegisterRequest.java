92feadc7d6fcb04a353ba13be5fbc832275ebc33
himanshusain0 <ht0257445@gmail.com>
2026-02-11 01:34:52 +0530

    UserService only user now

---
+++ userService/userService/src/main/java/com/fitness/userService/dto/RegisterRequest.java
@@ -0,0 +1,32 @@
+package com.fitness.userService.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class RegisterRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email format is invalid")
+    private String email;
+    @NotBlank(message = "Password is required")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&]).+$",
+            message = "Password must contain uppercase, lowercase, number and special character"
+    )
+    private String password ;
+
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 30, message = "First name must be between 2 and 30 characters")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 30, message = "Last name must be between 2 and 30 characters")
+    private String lastName;
+
+}
