8f327aba084a97ac8bd7a0747fadb64bae1934a0
himanshusain0 <ht0257445@gmail.com>
2026-02-15 01:25:52 +0530

    .

---
+++ aiservice/src/main/java/com/fitness/aiservice/model/Recommendation.java
@@ -0,0 +1,40 @@
+package com.fitness.aiservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "recommendation")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Recommendation {
+    @Id
+    private  String id;
+    private  String activityId;
+    private String userId;
+    private String activityType;
+
+    @Column(columnDefinition = "TEXT")
+    private String improvements;
+
+    @Column(columnDefinition = "TEXT")
+    private String suggestions;
+
+    @Column(columnDefinition = "TEXT")
+    private String safety;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
